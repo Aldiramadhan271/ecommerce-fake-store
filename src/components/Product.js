cc3ff0e942afb983d195ff4be67ca771c7753dd9
Aldiramadhan271 <ramadhanaldi60@gmail.com>
2023-05-18 15:04:29 +0700

    first commit

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, {useContext} from 'react';
+// import Link
+import { Link } from 'react-router-dom';
+// import icons
+import {BsPlus, BsEyeFill} from 'react-icons/bs'
 
-const Product = () => {
-  return <div>Product</div>;
+import { CartContext } from '../contexts/CartContext';
+
+const Product = ({product}) => {
+  const {addToCart} = useContext(CartContext)
+  // destructure product 
+  const { id, image, category, title, price } = product;
+  return (
+    <div>
+      <div className='border border-[#e4e4e4] h-[300px] mb-4 relative group overflow-hidden transition'>
+        <div className='w-full h-full flex justify-center items-center'>
+          {/* image */}
+          <div className='w-[200px] flex justify-center items-center mx-auto'>
+            <img 
+              className='max-h-[160px] group-hover:scale-110 transition duration-300' 
+              src={image} 
+              alt="" />
+          </div>
+        </div>
+        <div className='absolute top-6 -right-11 group-hover:right-5 bg-red-500/40 p-2 flex-col items-center justify-center gap-y-2 opacity-0 group-hover:opacity-100 transition-all duration-300'>
+          <button onClick={() => addToCart(product, id)}>
+            <div className='flex justify-center items-center text-white h-12 w-12 bg-red-500'>
+              <BsPlus className='text-3xl' />
+            </div>
+          </button>
+          <Link to={`/product/${id}`} className='w-12 h-12 bg-white flex justify-center items-center text-primary drop-shadow-xl'>
+            <BsEyeFill />
+          </Link>
+        </div>
+      </div>
+      {/* category & price */}
+      <div>
+        <div className='text-sm capitalize text-gray-500 mb-1'>{category}</div>
+        <Link to={`/product/${id}`}>
+          <h2 className='font-semibold mb-1'>{title}</h2>
+        </Link>
+        <div className='font-semibold'>$ {price}</div>
+      </div>
+    </div>
+  )
 };
 
 export default Product;
